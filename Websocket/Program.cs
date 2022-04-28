0e48bf9241e66b01f8c832aa9707c1502bfa5320
Iliraa <ilir.ruka@elev.ga.lbs.se>
2022-04-28 11:43:19 +0200

    j

--- Websocket/Program.cs
+++ Websocket/Program.cs
@@ -6,5 +6,26 @@ using Websocket;
 
 
 
-Client client = new Client();
-client.ExecuteClient();
+Console.WriteLine("1.server");
+Console.WriteLine("2.cleint");
+int a = Int32.Parse(Console.ReadLine()); 
+
+if(a == 1)
+{
+    Server();
+}
+else if (a == 2)
+{
+    Client();
+}
+static void Client()
+{
+    AsynchronousClient client = new AsynchronousClient();
+    client.Test();
+}
+static void Server()
+{
+    AsynchronousSocketListener server = new AsynchronousSocketListener();
+    server.Test();
+}
+
